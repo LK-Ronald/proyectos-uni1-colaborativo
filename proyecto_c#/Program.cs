65edd6d8792dd4c015c84b93324eebbee1cb76e7
LkRonald <rpereirac@unicartagena.co>
2026-08-23 16:25:16 -0500

    se agrego un bucle for para recorer el arreglo/lista

--- proyecto_c#/Program.cs
+++ proyecto_c#/Program.cs
@@ -9,6 +9,10 @@ namespace proyecto_cs
 
             int[] enteros = [3, 4, 76, 4, 23, 35, 65, 67, 87, 23];
 
+            for (int i = 0; i < enteros.Length; i++)
+            {
+                Console.WriteLine(enteros[i]);
+            }
 
         }
     }
--- proyecto_dart/bin/proyecto_dart.dart
+++ proyecto_dart/bin/proyecto_dart.dart
@@ -1,5 +1,7 @@
 void main() {
-
   List<int> enteros = [3, 4, 76, 4, 23, 35, 65, 67, 87, 23];
-  
+
+  for (int i = 0; i < enteros.length; i++) {
+    print(enteros[i]);
+  }
 }
