f4ebf7f7e954a7c79a530ad40e4ca044980a7b8a
LkRonald <rpereirac@unicartagena.co>
2026-08-23 16:36:16 -0500

    se recorre el array/lista con un bucle foreach

--- proyecto_dart/bin/proyecto_dart.dart
+++ proyecto_dart/bin/proyecto_dart.dart
@@ -1,7 +1,7 @@
 void main() {
   List<int> enteros = [3, 4, 76, 4, 23, 35, 65, 67, 87, 23];
 
-  for (int i = 0; i < enteros.length; i++) {
-    print(enteros[i]);
+  for (int i in enteros) {
+    print(i);
   }
 }
