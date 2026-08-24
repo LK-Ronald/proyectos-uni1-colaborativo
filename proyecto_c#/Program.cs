75373e85a1b8a6f7aab87df23f78cedc4907aaf9
LkRonald <rpereirac@unicartagena.co>
2026-08-23 20:57:21 -0500

    se creo la matriz y se inicializo del 1 al

--- proyecto_c#/Program.cs
+++ proyecto_c#/Program.cs
@@ -7,6 +7,12 @@ namespace proyecto_cs
         static void Main()
         {
 
+            int[][] matriz = [
+                [1,2,3],
+                [4,5,6],
+                [7,8,9]
+            ];
+
         }
     }
 
